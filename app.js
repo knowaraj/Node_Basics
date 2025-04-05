9537ecb0b2e8c7d477505a6e548a1eeb2e22c81c
knowaraj <sendmailtoaraj@gmail.com>
2025-04-05 17:37:28 +0545

    status code and database string

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ console.log("Hello")
 const express = require('express')
 const app = express()
 
+const ConnectionString = "mongodb+srv://knowaraj:know@r@j@cluster0.cqdthg3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 app.get("/",(req,res) => {
     res.send("Hello Response")
 })
-app.get("/check", (req,res) => {
-    res.send("ChecK")
-})
+
+// app.get("/check", (req,res) => {
+//     res.status(200).json({
+//         "name" : "araj",
+//         "age" : 19
+//     })
+// })
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
