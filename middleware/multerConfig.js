5a9c8a128375f33c946551a594ed4f8c58f2fb51
knowaraj <sendmailtoaraj@gmail.com>
2025-05-06 23:01:11 +0545

    filetype validate

--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -2,7 +2,12 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination : function(req,file,cb){
-    cb(null,'./storage')//(error,success)
+        const allowed = ['image/png','image/jpg']
+        if(!allowed.includes(file.mimetype)){
+            cb(new Error("this is not supported"))
+            return
+        }
+        cb(null,'./storage')//(error,success)
     },
     filename : function(req,file,cb){
         cb(null,Date.now()+ "_"+file.originalname)
